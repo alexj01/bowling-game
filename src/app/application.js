ec53843e38edde168e8942519cc3a67d7bbf446a
Alex Janus <alexander_janus@homedepot.com>
2017-09-14 14:26:43 -0400

    building method skeleton

--- src/app/application.js
+++ src/app/application.js
@@ -1,3 +1,50 @@
-exports.gameStart = () => {
-    return 'Hello! Welcome to the Bowling Game.';
-};
+class BowlingGame {
+    constructor(){
+        this.playerCount;
+        this.players = [];
+        this.rolls = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,10]
+        this.frame = 0;
+    }
+
+    greet() {
+        return 'Hello! Welcome to the Bowling Game.'        
+    }
+
+    setPlayerCount(numberOfPlayers) {
+        this.playerCount = parseInt(numberOfPlayers)
+    }
+
+    getPlayerCount() {
+        return this.playerCount;
+    }
+
+    generatePlayers() {
+        for(let i = 1; i <= this.getPlayerCount(); i ++) {
+            this.players.push({'playerId': i, 'score': 0})
+        }
+    }
+
+    getPlayer(playerId) {
+        return this.players[playerId];
+    }
+
+    nextFrame() {
+        this.frame++;
+    }
+
+    getCurrentFrame() {
+        return this.frame;
+    }
+
+    roll(pins, playerId) {
+        let player = this.getPlayer(playerId)
+        player.score += pins;
+    }
+
+    getPlayerScore(playerId) {
+        let player = this.getPlayer(playerId)
+        return player.score;
+    }
+}
+
+module.exports = BowlingGame;
--- test/application-test.js
+++ test/application-test.js
@@ -4,16 +4,41 @@ let assert = chai.assert;
 let should = chai.should();
 let expect = chai.expect;
 
-import application from '../src/app/application';
+import BowlingGame from '../src/app/application';
 
 describe('Application Tests', () => {
 
     describe('Initialization', () => {
-        
-        let greeting = application.gameStart();
+        let game = new BowlingGame();
+        let greeting = game.greet();
+        game.setPlayerCount(2)
+        game.generatePlayers()
         
         it('should say hello when started', () => {
             expect(greeting).to.equal('Hello! Welcome to the Bowling Game.')
+        })
+        
+        it('should accept an input of the number of players', () => {
+            expect(game.getPlayerCount()).to.equal(2)
+        })
+        
+        it('should generate players to match the player count', () => {
+            expect(game.players.length).to.equal(game.getPlayerCount())
+        })
+
+        it('should advance the frame', () => {
+            expect(game.getCurrentFrame()).to.equal(0)
+
+            game.nextFrame()
+
+            expect(game.getCurrentFrame()).to.equal(1)
+        })
+
+        it('should update the score after a roll', () => {
+            let pins = 4;
+            let playerId = 1
+            game.roll(pins,playerId)
+            expect(game.getPlayerScore(playerId)).to.equal(pins)
         })
 
     })
