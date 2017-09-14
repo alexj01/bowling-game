df6697bbcd7da9c834e493468dc4d0e2d20f4a38
Alex Janus <alexander_janus@homedepot.com>
2017-09-14 16:32:11 -0400

    building method skeleton

--- src/app/application.js
+++ src/app/application.js
@@ -2,7 +2,6 @@ class BowlingGame {
     constructor(){
         this.playerCount;
         this.players = [];
-        this.rolls = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,10]
         this.frame = 0;
     }
 
@@ -37,14 +36,15 @@ class BowlingGame {
     }
 
     roll(pins, playerId) {
-        let player = this.getPlayer(playerId)
-        player.score += pins;
+        this.getPlayer(playerId).score += pins;
     }
 
     getPlayerScore(playerId) {
         let player = this.getPlayer(playerId)
         return player.score;
     }
+
+
 }
 
 module.exports = BowlingGame;
